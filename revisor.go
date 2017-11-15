325b1e82d29152784363a08e95fed2bbebe44c1b
Nikolai Krapivchenko <krapivchenkon@gmail.com>
2017-11-15 07:24:57 +0100

    Fixed gometaliner warnings

--- revisor.go
+++ revisor.go
@@ -15,8 +15,8 @@ func SetSomeOption(opt string) option {
 
 // NewRequestVerifier returns a function that can be used to verify if request
 // satisfies OpenAPI definition constraints
-func NewRequestVerifier(definition string, options ...option) (func(*http.Request) error, error) {
-	a := apiVerifier{name: definition}
+func NewRequestVerifier(definitionPath string, options ...option) (func(*http.Request) error, error) {
+	a := newAPIVerifier(definitionPath)
 	a.setOptions(options...)
 	return a.verifyRequest, nil
 }
@@ -25,8 +25,8 @@ func NewRequestVerifier(definition string, options ...option) (func(*http.Reques
 // satisfies OpenAPI definition constraints. Two perform such alidation we need
 // to specify context in which current response was received, and that is represented
 // by method and url paramters
-func NewResponseVerifier(definition, method, url string, options ...option) (func(*http.Response) error, error) {
-	a := apiVerifier{name: definition}
+func NewResponseVerifier(definitionPath, method, url string, options ...option) (func(*http.Response) error, error) {
+	a := newAPIVerifier(definitionPath)
 	a.setOptions(options...)
 	return func(res *http.Response) error {
 		return a.verifyResponse(method, url, res)
@@ -35,17 +35,27 @@ func NewResponseVerifier(definition, method, url string, options ...option) (fun
 
 // NewVerifier returns a function that can be used to verify both - a request
 // and the response made in the context of the request
-func NewVerifier(definition string, options ...option) (func(*http.Request, *http.Response) error, error) {
-	a := apiVerifier{name: definition}
+func NewVerifier(definitionPath string, options ...option) (func(*http.Request, *http.Response) error, error) {
+	a := newAPIVerifier(definitionPath)
 	a.setOptions(options...)
 	return a.verify, nil
+}
+
+func newAPIVerifier(definitionPath string) *apiVerifier {
+	mapper := newSimpleMapper(map[string][]string{})
+	a := &apiVerifier{
+		name:   definitionPath,
+		mapper: mapper,
+	}
+	return a
 }
 
 // apiVerifier implements various verification functions and encloses various
 // verification options as well as an OpenAPI Document
 type apiVerifier struct {
-	name string
-	opt  string
+	name   string
+	opt    string
+	mapper *simpleMapper
 }
 
 // verifyRequest verifies if request is valid according to OpenAPI definition
